0bd6649387491b5d1f087c2881b12f91a51ad99b
Kohei Suzuki <eagletmt@gmail.com>
2020-11-23 23:59:40 +0900

    Change current directory to build c-bindings

--- build.rs
+++ build.rs
@@ -1,9 +1,19 @@
 fn main() {
-    gobuild::Build::new()
-        .buildmode(gobuild::BuildMode::CArchive)
-        .file("./vendor/go-jsonnet/c-bindings")
-        .compile("gojsonnet");
     let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let c_bindings = "vendor/go-jsonnet/c-bindings";
+    println!("cargo:rerun-if-changed={}", c_bindings);
+    println!("cargo:rustc-link-lib=static=gojsonnet");
+    println!("cargo:rustc-link-search=native={}", out_dir.display());
+    let status = std::process::Command::new("go")
+        .current_dir(c_bindings)
+        .arg("build")
+        .arg("-buildmode=c-archive")
+        .arg(format!("-o={}/libgojsonnet.a", out_dir.display()))
+        .status()
+        .expect("Failed to launch Go compiler");
+    if !status.success() {
+        panic!("Failed to build libgojsonnet.a");
+    }
     std::fs::copy(
         "vendor/go-jsonnet/c-bindings/internal.h",
         out_dir.join("internal.h"),
