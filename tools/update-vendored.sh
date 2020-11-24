f64a9fd3717a3a17ec774203f9386252f6d88650
Kohei Suzuki <eagletmt@gmail.com>
2020-11-25 01:42:34 +0900

    Update go-jsonnet version to 57e6137c936549b3df96181d5741328f01946faf

--- tools/update-vendored.sh
+++ tools/update-vendored.sh
@@ -2,10 +2,12 @@
 set -ex
 
 jsonnet_version=0.17.0
+# XXX: Use thread-safe version for now: https://github.com/google/go-jsonnet/commit/57e6137c936549b3df96181d5741328f01946faf
+gojsonnet_version=57e6137c936549b3df96181d5741328f01946faf
 
 rm -rf vendor/go-jsonnet
 mkdir -p vendor/go-jsonnet
-curl -sSfL https://github.com/google/go-jsonnet/archive/v${jsonnet_version}.tar.gz | tar -x --directory=vendor/go-jsonnet --gzip --strip-components=1 --file=-
+curl -sSfL https://github.com/google/go-jsonnet/archive/${gojsonnet_version}.tar.gz | tar -x --directory=vendor/go-jsonnet --gzip --strip-components=1 --file=-
 (cd vendor/go-jsonnet && go mod vendor)
 
 # Need google/jsonnet/include/libjsonnet.h until https://github.com/google/go-jsonnet/pull/482
